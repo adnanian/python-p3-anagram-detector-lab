277d4d31ac6531841494fa6e8cae548b9754b65e
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-28 17:32:47 -0600

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    pass
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, word_list):
+        pass
+        anagrams = []
+        for word in word_list:
+            if sorted(self.word.lower()) == sorted(word.lower()):
+                anagrams.append(word)
+        return anagrams
